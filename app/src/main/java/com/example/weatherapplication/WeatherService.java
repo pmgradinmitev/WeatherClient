18b4df448c717df2597deb7495f73f64b28818ce
pmgradinmitev <pmg.tadinmitev92@gmail.com>
2025-03-10 17:43:14 +0200

    Added comments to: WeatherService.java

--- app/src/main/java/com/example/weatherapplication/WeatherService.java
+++ app/src/main/java/com/example/weatherapplication/WeatherService.java
@@ -4,6 +4,17 @@ import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface WeatherService {
+    /**
+     *
+     * @param city
+     * @param apiKey
+     * @param units
+     * @return
+     *
+     * Call<T>: Represents an HTTP request that can be executed asynchronously or synchronously.
+     * @GET: Annotation used to define a GET request.
+     * @Query: Used to append query parameters to the request URL.
+     */
     @GET("weather")
     Call<WeatherResponse> getWeather(
             @Query("q") String city,
